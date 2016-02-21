e2762c6c1ea67984b65542a2c9d2259cd00f9714
Andi Miller <andi@andimiller.net>
2016-02-21 03:34:00 +0000

    fix building LocationManager

--- src/main/scala/moe/pizza/auth/plugins/LocationManager.scala
+++ src/main/scala/moe/pizza/auth/plugins/LocationManager.scala
@@ -3,12 +3,14 @@ package moe.pizza.auth.plugins
 import moe.pizza.auth.models.Pilot
 import moe.pizza.crestapi.CrestApi
 
+import scala.concurrent.ExecutionContext
+
 /**
   * Created by Andi on 21/02/2016.
   */
 object LocationManager {
 
-  def locateUsers(crest: CrestApi)(pilots: List[Pilot]) = {
+  def locateUsers(crest: CrestApi)(pilots: List[Pilot])(implicit ec: ExecutionContext) = {
     pilots.map { p =>
       p.getCrestTokens.map { token =>
         (p, crest.character.location(token.characterID, token.token))
