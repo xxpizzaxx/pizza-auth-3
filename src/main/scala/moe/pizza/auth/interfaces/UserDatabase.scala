f9373fdc2aa5f0066079ff81dbbfa96a2892417a
Andi Miller <andi@andimiller.net>
2016-02-22 01:07:50 +0000

    add interface for userdatabase

---
+++ src/main/scala/moe/pizza/auth/interfaces/UserDatabase.scala
@@ -0,0 +1,16 @@
+package moe.pizza.auth.interfaces
+
+import moe.pizza.auth.models.Pilot
+
+/**
+  * Created by Andi on 21/02/2016.
+  */
+trait UserDatabase {
+  def addUser(p: Pilot): Boolean
+  def deleteUser(p: Pilot): Boolean
+  def updateUser(p: Pilot): Boolean
+  def authenticateUser(uid: String, password: String): Option[Pilot]
+  def getUser(uid: String): Option[Pilot]
+  def setPassword(p: Pilot, password: String): Boolean
+  def getUsers(filter: String): List[Pilot]
+}
