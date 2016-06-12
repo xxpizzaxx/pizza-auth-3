ed64cbb938e405d3b71eb7f9e5752c0d85b64ade
Andi Miller <andi@andimiller.net>
2016-06-12 01:45:31 +0100

    finish rewrite of SessionManager to be stateless

--- src/main/scala/moe/pizza/auth/webapp/SessionManager.scala
+++ src/main/scala/moe/pizza/auth/webapp/SessionManager.scala
@@ -21,45 +21,31 @@ object SessionManager {
   val SESSION = AttributeKey[Session2]("SESSION")
   val SESSIONID = AttributeKey[Session2]("SESSIONID")
   val LOGOUT = AttributeKey[String]("LOGOUT")
-  val COOKIESESSION = "authsessionid"
+  val COOKIESESSION = "authsession"
 }
 
 class SessionManager(secretKey: String) extends HttpMiddleware {
   val log = LoggerFactory.getLogger(getClass)
-  val sessions = TrieMap[String, Session2]()
 
-  implicit def jodainstant2javainstant(jodai: org.joda.time.Instant): Instant = Instant.parse(jodai.toString)
-
-  def newSession(): (String, Session2) = {
-    val sessionid = UUID.randomUUID().toString
-    val session = new Session2(List.empty, None)
-    val claim = JwtClaim(
-      expiration = Some(Instant.now.plusSeconds(86400*30).getEpochSecond), // lasts 30 days
-      issuedAt = Some(Instant.now.getEpochSecond)
-    ) +("id", sessionid)
-    val token = JwtCirce.encode(claim, secretKey, JwtAlgorithm.HS256)
-    (sessionid, session)
-  }
-
-  case class MyJwt(exp: Long, iat: Long, id: String)
+  case class MyJwt(exp: Long, iat: Long, session: Session2)
 
   override def apply(s: HttpService): HttpService = Service.lift { req =>
     log.info(s"Intercepting request ${req}")
     val processedHeaders = req.headers.get(headers.Cookie).toList.flatMap(_.values.list).map { header =>
       val session = JwtCirce.decodeJson(header.content, secretKey, Seq(JwtAlgorithm.HS256)).toOption.flatMap { jwt =>
         jwt.as[MyJwt].toOption
-      }.flatMap{ myjwt =>
-        sessions.get(myjwt.id).map(x => (myjwt.id, x))
+      }.map{ myjwt =>
+        myjwt.session
       }
-      (header, session)
+      session
     }
-    val (successful, failed) = processedHeaders.partition(_._2.isDefined)
+    val (successful, failed) = processedHeaders.partition(_.isDefined)
     log.info("Finished processing headers")
     log.info(s"Successful sessions: ${successful}")
     log.info(s"Failed sessions: ${failed}")
 
     // if we didn't find a valid session, make them one
-    val (sessionid, session) = successful.headOption.flatMap(_._2).getOrElse(newSession())
+    val session = successful.headOption.getOrElse(Session2(List.empty, None))
     val removeme = successful.tail ++ failed
 
     // do the inner request
@@ -67,24 +53,24 @@ class SessionManager(secretKey: String) extends HttpMiddleware {
 
     response.map { resp =>
       // do all of this once the request has been created
-      resp.attributes.get(SESSION).getOrElse(session)
-    }
-
-    response.map { resp =>
-      log.info("checking inner Response for sessions")
-      resp.attributes.get(SESSION) match {
-        case Some(newsession) =>
-          log.info(s"Session found, storing it in the map: $id => $newsession")
-          sessions.put(id, newsession)
-        case None =>
-          log.info("No session found")
-          //sessions.remove(id)
-      }
-      resp.attributes.get(LOGOUT) match {
-        case Some(_) =>
-          log.info("logout flag set, logging out")
-          resp.removeCookie(COOKIESESSION)
-        case None => resp
+      val sessionToSave = resp.attributes.get(SESSION).getOrElse(session)
+      val oldsessions = resp.headers.get(headers.Cookie).toList.flatMap(_.values.list).filter(_.name == COOKIESESSION)
+      val respWithCookieRemovals = oldsessions.foldLeft(resp){ (resp, cookie) => resp.removeCookie(cookie)}
+      if (resp.attributes.get(LOGOUT).isEmpty) {
+        log.info(s"saving the session as a cookie")
+        val claim = JwtClaim(
+          expiration = Some(Instant.now.plusSeconds(86400*30).getEpochSecond), // lasts 30 days
+          issuedAt = Some(Instant.now.getEpochSecond)
+        ) +("session", sessionToSave)
+        val token = JwtCirce.encode(claim, secretKey, JwtAlgorithm.HS256)
+        respWithCookieRemovals.addCookie(
+          COOKIESESSION,
+          token,
+          None
+        )
+      } else {
+        log.info(s"log out flag was seto, not saving any cookies")
+        respWithCookieRemovals
       }
     }
   }
