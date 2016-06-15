93ae51e0eb930076dd6b96bbfd221f426bafb0a2
Andi Miller <andi@andimiller.net>
2016-06-16 00:45:59 +0100

    make the XMPP broadcaster vhost aware

--- src/main/scala/moe/pizza/auth/adapters/XmppBroadcastService.scala
+++ src/main/scala/moe/pizza/auth/adapters/XmppBroadcastService.scala
@@ -9,13 +9,20 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class XmppBroadcastService(host: String, password: String) extends BroadcastService {
 
+  def getJabberServer(u: Pilot): String = u.accountStatus match {
+    case Pilot.Status.internal => host
+    case Pilot.Status.ally => s"allies.$host"
+    case Pilot.Status.ineligible => s"public.$host"
+    case _ => "none"
+  }
+
   val xmppbot = new XmppBot
   xmppbot.connect("pingbot", host, password)
 
   override def sendAnnouncement(msg: String, from: String, to: List[Pilot]): Future[Int] = {
     Future {
       to.map { p =>
-        xmppbot.sendMessage(s"${p.uid}@$host", msg)
+        xmppbot.sendMessage(s"${p.uid}@${getJabberServer(p)}", msg)
         1
       }.sum
     }
