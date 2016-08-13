9f60bf1885722cd8318e30fc5f40a63cbf47689b
Andi Miller <andi@andimiller.net>
2016-08-13 19:45:35 +0100

    fail gracefully when doing an update

--- src/main/scala/moe/pizza/auth/tasks/Update.scala
+++ src/main/scala/moe/pizza/auth/tasks/Update.scala
@@ -6,22 +6,24 @@ import moe.pizza.crestapi.CrestApi
 import moe.pizza.eveapi._
 
 import scala.concurrent.ExecutionContext
+import scala.util.Try
 
 
 class Update(crest: CrestApi, eveapi: EVEAPI, chain: PilotGrader)(implicit val ex: ExecutionContext) {
 
-  def updateUser(p: Pilot) = {
+  def updateUser(p: Pilot): Pilot = {
     val keys = p.getCrestTokens
     val mainkey = keys.head
-    val charinfo = eveapi.eve.CharacterInfo(mainkey.characterID).sync()
-    val refreshed = crest.refresh(mainkey.token).sync()
-    val corpAndAlliance = charinfo match {
-      case Left(r) => (r.result.corporation, "")
-      case Right(r) => (r.result.corporation, r.result.alliance)
-    }
-    val pilotWithUpdatedMembership = p.copy(corporation = corpAndAlliance._1, alliance = corpAndAlliance._2)
-    val gradedPilot = pilotWithUpdatedMembership.copy(accountStatus = chain.grade(pilotWithUpdatedMembership))
-    gradedPilot
+    Try {
+      val charinfo = eveapi.eve.CharacterInfo(mainkey.characterID).sync()
+      val refreshed = crest.refresh(mainkey.token).sync()
+      val corpAndAlliance = charinfo match {
+        case Left(r) => (r.result.corporation, "")
+        case Right(r) => (r.result.corporation, r.result.alliance)
+      }
+      val pilotWithUpdatedMembership = p.copy(corporation = corpAndAlliance._1, alliance = corpAndAlliance._2)
+      val gradedPilot = pilotWithUpdatedMembership.copy(accountStatus = chain.grade(pilotWithUpdatedMembership))
+      gradedPilot
   }
 
 }
