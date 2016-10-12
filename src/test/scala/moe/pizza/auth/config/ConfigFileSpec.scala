99e931c5461ca753496a321b323a1945a4b87fef
Andi Miller <andi@andimiller.net>
2016-10-12 16:02:23 +0100

    fix unit test that read the wrong config file

--- src/test/scala/moe/pizza/auth/config/ConfigFileSpec.scala
+++ src/test/scala/moe/pizza/auth/config/ConfigFileSpec.scala
@@ -8,7 +8,7 @@ import org.scalatest.{FlatSpec, MustMatchers}
 class ConfigFileSpec extends FlatSpec with MustMatchers {
 
   "ConfigFile" should "represent a .yml file" in {
-    val c = Main.parseConfigFile(new File("config.yml"))
+    val c = Main.parseConfigFile(new File("src/test/resources/config.yml"))
     c.isSuccess must equal(true)
     c.get.auth.alliance must equal("Confederation of xXPIZZAXx")
     c.get.auth.restkeys must equal(List("insertakeyhere"))
